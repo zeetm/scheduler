08d3c1a321a687ae05297678fa00b3706fdec326
Zeinab <zeinabtmohamed@gmail.com>
2022-02-28 13:19:48 -0500

    remove if statement, console.logs. And add conditional clases

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,32 +1,19 @@
 import React from "react";
 import "components/DayListItem.scss";
-import classNames from "classnames";
+import classnames from "classnames";
 
 
 export default function DayListItem(props) {
-const dayClasses = function () {
-  console.log('props.selected', props.selected)
-  console.log('props.spots', props.spots)
-
-  if (
-    props.selected === true
-  ){
-    return 'day-list__item--selected'
-  }
-  if (
-    props.spots === 0
-  ){
-    return 'day-list__item--full'
-  }
-
-  return "day-list__item"
-}
-const dayClass = dayClasses()
+  
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected" : props.selected,
+    "day-list__item--full": props.spots === 0
+  })
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
   );
-}
+ }
