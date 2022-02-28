b478c1fc1b4ce6ddd4e629958e4c0c87a83db4c7
Zeinab <zeinabtmohamed@gmail.com>
2022-02-28 13:03:52 -0500

    add DayListItem stories

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,30 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
 
 export default function DayListItem(props) {
+const dayClasses = function () {
+  console.log('props.selected', props.selected)
+  console.log('props.spots', props.spots)
+
+  if (
+    props.selected === true
+  ){
+    return 'day-list__item--selected'
+  }
+  if (
+    props.spots === 0
+  ){
+    return 'day-list__item--full'
+  }
+
+  return "day-list__item"
+}
+const dayClass = dayClasses()
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "index.scss";
-
 import Application from "components/Application";
+import DayListItem from "components/DayListItem";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
+
+storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  }) // Provides the default background color for our component
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />) // To define our stories, we call add() once for each of our test states to generate a story
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
+  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
+  ));
