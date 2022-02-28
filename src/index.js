49f2085fcce0cb78fe0dee52b9a771d9fc03f478
Zeinab <zeinabtmohamed@gmail.com>
2022-02-28 16:06:44 -0500

    add DayList stories

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom";
 import "index.scss";
 import Application from "components/Application";
 import DayListItem from "components/DayListItem";
+import DayList from "components/DayList";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
+
+//DayListItem stories//
 
 storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
   .addParameters({
@@ -16,3 +19,36 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
   .add("Clickable", () => (
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
   ));
+
+  //DayList stories//
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
+  
+  storiesOf("DayList", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+    })
+    .add("Monday", () => (
+      <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+    ))
+    .add("Tuesday", () => (
+      <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+    ))
+    .add("Wednesday", () => (
+        <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+    ));
