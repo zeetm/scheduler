6855a2185244bc16d645dfefb51e4478175ad049
Zeinab <zeinabtmohamed@gmail.com>
2022-02-28 18:50:21 -0500

    create InterviewListItem component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function InterviewerListItem() {
+
+return (
+<li className="interviewers__item">
+  <img
+    className="interviewers__item-image"
+    src="https://i.imgur.com/LpaY82x.png"
+    alt="Sylvia Palmer"
+  />
+  Sylvia Palmer
+</li>
+  );
+}
+
+export default InterviewerListItem
