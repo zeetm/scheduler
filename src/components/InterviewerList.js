b1e5ad75d933f1e6ce3aed3da600aa8424dee100
Zeinab <zeinabtmohamed@gmail.com>
2022-02-28 21:50:21 -0500

    create InterviewerList component and add styles

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classNames from "classnames";
+
+export default function InterviewerListItem () {
+return(
+<section className="interviewers">
+  <h4 className="interviewers__header text--light">Interviewer</h4>
+  <ul className="interviewers__list"></ul>
+</section>
+);
+}
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,28 +1,25 @@
 import React from "react";
+import classnames from "classnames";
 import "components/InterviewerListItem.scss";
 
-const classNames = require('classnames');
+export default function InterviewerListItem(props) {
 
-export default function InterviewListItem(props) {
-
-  const interviewerClass = classNames('interviewers__item', {
-    'interviewers__item--selected': props.selected
-  });
-
-  const name = props.name
+  const interviewerClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  })
 
   return (
     <li 
       className={interviewerClass}
-      onClick = {() => props.setInterviewer(props.name)}>
+      onClick={() => props.setInterviewer(props.id)}  
+    >
       <img
-        className="interviewers__item-image"
-        src={ props.avatar }
-        alt={ name }
+        className={"interviewers__item-image"}
+        src={props.avatar}
+        alt={props.name}
       />
-    {props.selected && (
-      name
-    )}
+      {props.name}
     </li>
-  )
-};
+  );
+}
+
