83f705c0005994fcd9a6ecc55f27bd4653bbfb23
Zeinab <zeinabtmohamed@gmail.com>
2022-03-01 21:11:25 -0500

    Import components/Appointment into Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "components/Appointment"
+
 
 const days = [
   {
