c635a795129cb6d99612f9be977f006f87cee86d
Zeinab <zeinabtmohamed@gmail.com>
2022-03-01 19:36:08 -0500

    add Error stories

--- stories/index.js
+++ stories/index.js
@@ -17,15 +17,18 @@ import InterviewerList from "components/InterviewerList";
 
 import Appointment from "components/Appointment/index.js";
 
-import Header from "components/Appointment/Header"
+import Header from "components/Appointment/Header";
 
-import Empty from "components/Appointment/Empty"
+import Empty from "components/Appointment/Empty";
 
-import Show from "components/Appointment/Show"
+import Show from "components/Appointment/Show";
 
-import Confirm from "components/Appointment/Confirm"
+import Confirm from "components/Appointment/Confirm";
 
 import Status from "components/Appointment/Status";
+
+import Error from "components/Appointment/Error";
+
 
 //Button Stories
 
@@ -174,4 +177,6 @@ storiesOf("Appointment", module)
   .add("Show", () => (<Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")}/>))
   .add("Confirm", () => (<Confirm message="Delete the appointment?" onCancel={action("onCancel")} onConfirm={action("onConfirm")}/>))
   .add("Status", () => (<Status message="Deleting"/>))
+  .add("Error", () => (<Error message="Could not delete appointment" onClose={action("onClose")}/>))
+
   
