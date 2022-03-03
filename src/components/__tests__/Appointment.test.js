315275777e85a2860248fa4c15c2b99c6e20e8f0
Zeinab <zeinabtmohamed@gmail.com>
2022-03-03 10:01:46 -0500

    add first test

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Appointment from "components/Appointment/index";
+
+
+describe("Appointment", () => {
+
+it("renders without crashing", () => {
+  render(<Appointment />);
+});
+
+it("does something it is supposed to do", () => {
+  // ...
+});
+
+it("does something else it is supposed to do", () => {
+  // ...
+});
+});
