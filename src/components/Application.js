7b30e6a86ab185742a862a156709a83fd3961164
Zeinab <zeinabtmohamed@gmail.com>
2022-03-02 20:16:26 -0500

    Create a new file hooks/useApplicationData.js and move the logic used to manage the state from the components/Application.js into it

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,129 +1,60 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "components/Application.scss";
-import DayList from "./DayList";
 import Appointment from "components/Appointment"
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "./helpers/selectors";
+import useApplicationData from "./hooks/useApplicationData";
+import DayList from "./DayList";
 
 
 export default function Application(props) {
+const {
+  state,
+  setDay,
+  bookInterview,
+  cancelInterview
+} = useApplicationData();
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  })
-
-  const setDay = day => setState({...state, day});
-
-  const appointments = getAppointmentsForDay(state, state.day);console.log(appointments);
-  
-  //const interviewers = getInterviewersForDay(state, state.day);
-
-  //bookInterview function
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview}
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-  
-    return axios.put(`/api/appointments/${id}`, appointment)
-    .then(response => {
-      setState({
-        ...state,
-        appointments
-      })
-    })
-  
-  };
-
-  //cancelInterview function
-  function cancelInterview(id) {
-    const nullAppointment = {
-      ...state.appointments[id],
-      interview: null
-    }
-    const appointments = {
-      ...state.appointments,
-      [id]: nullAppointment
-    };
-  
-    return axios.delete(`api/appointments/${id}`)
-    .then(response => {
-      setState({
-        ...state,
-        appointments
-      })
-    })
-  }
+const appointmentList = getAppointmentsForDay(state, state.day).map( appointment => {
+  return(
+    <Appointment 
+      key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={getInterview(state, appointment.interview)}
+      interviewers={getInterviewersForDay(state, state.day)}
+      bookInterview={bookInterview}
+      cancelInterview={cancelInterview}
+    />
+  );
+});
 
 
-  const appointmentList = appointments.map( appointment => {
-    const interview = getInterview(state, appointment.interview)
-  
-    return(
-      <Appointment 
-        key={appointment.id}
-        id={appointment.id}
-        time={appointment.time}
-        interview={getInterview(state, appointment.interview)}        interviewers={getInterviewersForDay(state, state.day)}
-        bookInterview={bookInterview}
-        cancelInterview={cancelInterview}
-      />
-    );
-  })
-
-
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers")
-    ])
-    .then(response => {
-      setState(prev => ({
-        ...prev, 
-        days: response[0].data,
-        appointments: response[1].data,
-        interviewers: response[2].data
-      }))
-    });
-  }, [])
-
-
-  return (
-    <main className="layout">
-      <section className="sidebar">
-          <img
+return (
+  <main className="layout">
+    <section className="sidebar">
+      <img
           className="sidebar--centered"
           src="images/logo.png"
           alt="Interview Scheduler"
         />
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu">
-          <DayList
+      <hr className="sidebar__separator sidebar--centered" />
+      <nav className="sidebar__menu">
+        <DayList
           days={state.days}
           day={state.day}
           setDay={setDay}
-          />
-        </nav>
-        <img
-          className="sidebar__lhl sidebar--centered"
-          src="images/lhl.png"
-          alt="Lighthouse Labs"
         />
-      </section>
-      <section className="schedule">
-        {appointmentList}
-        <Appointment key="last" time="5pm" />
-      </section>
-    </main>
-  );
+      </nav>
+      <img
+        className="sidebar__lhl sidebar--centered"
+        src="images/lhl.png"
+        alt="Lighthouse Labs"
+      />
+    </section>
+    <section className="schedule">
+      {appointmentList}
+      <Appointment key="last" time="5pm" />
+    </section>
+  </main>
+);
 }
-
